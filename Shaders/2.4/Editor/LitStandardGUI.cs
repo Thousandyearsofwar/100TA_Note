7bf81102183c2a032ceb4101d63da7b6015e481f
thousandyearsofwar <46675384+Thousandyearsofwar@users.noreply.github.com>
2021-06-22 01:21:44 +0800

    所罗门，我回来了啊(

---
+++ Shaders/2.4/Editor/LitStandardGUI.cs
@@ -0,0 +1,314 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace UnityEditor.Rendering.Universal.ShaderGUI
+{
+    public enum WorkflowMode
+    {
+        Specular = 0,
+        Metallic
+    }
+
+    public enum SmoothnessMapChannel
+    {
+        SpecularMetallicAlpha,
+        AlbedoAlpha,
+    }
+
+    public enum LightMode
+    {
+        Classic_Lighting = 0,
+        //Further Version will update it
+        PBR = 1
+
+    }
+    public static class LitStandardGUI
+    {
+        public static class Styles
+        {
+            public static GUIContent AlbedoMapTip = new GUIContent("AlbedoTex", "漫反射贴图");
+            public static GUIContent NormalMapTip = new GUIContent("NormalTex", "法线贴图");
+            public static GUIContent SpecularMapTip = new GUIContent("SpecularTex", "镜面反射贴图");
+            public static GUIContent EmissionMapTip = new GUIContent("EmissionTex", "自发光贴图");
+
+            public static GUIContent AlbedoTip = new GUIContent("Albedo", "漫反射率");
+            public static GUIContent SpecularTip = new GUIContent("Specular", "镜面反射率");
+            public static GUIContent NormalScaleTip = new GUIContent("NormalScale", "法线比例");
+            public static GUIContent GlossTip = new GUIContent("Gloss", "高光度");
+
+            public static GUIContent LightModeTip = new GUIContent("LightMode", "光照模型");
+            public static readonly string[] LightModeToggleText = { "经典光照", "PBR" };
+
+            //@@@ PBR use
+            public static GUIContent workflowModeText = new GUIContent("Workflow Mode",
+    "Select a workflow that fits your textures. Choose between Metallic or Specular.");
+
+            public static GUIContent specularMapText =
+                new GUIContent("Specular Map", "Sets and configures the map and color for the Specular workflow.");
+
+            public static GUIContent metallicMapText =
+                new GUIContent("Metallic Map", "Sets and configures the map for the Metallic workflow.");
+
+            public static GUIContent smoothnessText = new GUIContent("Smoothness",
+                "Controls the spread of highlights and reflections on the surface.");
+
+            public static GUIContent smoothnessMapChannelText =
+                new GUIContent("Source",
+                    "Specifies where to sample a smoothness map from. By default, uses the alpha channel for your map.");
+
+            public static GUIContent highlightsText = new GUIContent("Specular Highlights",
+                "When enabled, the Material reflects the shine from direct lighting.");
+
+            public static GUIContent reflectionsText =
+                new GUIContent("Environment Reflections",
+                    "When enabled, the Material samples reflections from the nearest Reflection Probes or Lighting Probe.");
+
+            public static GUIContent occlusionText = new GUIContent("Occlusion Map",
+                "Sets an occlusion map to simulate shadowing from ambient lighting.");
+
+            public static readonly string[] metallicSmoothnessChannelNames = { "Metallic Alpha", "Albedo Alpha" };
+            public static readonly string[] specularSmoothnessChannelNames = { "Specular Alpha", "Albedo Alpha" };
+            //
+        }
+
+        public struct LitStandardProperties
+        {
+            public MaterialProperty LightModeToggle;
+            public MaterialProperty AlbedoMap;
+            public MaterialProperty NormalMap;
+            public MaterialProperty SpecularMap;
+            public MaterialProperty EmissionMap;
+            public MaterialProperty Albedo;
+            public MaterialProperty NormalScale;
+            public MaterialProperty Gloss;
+            //public MaterialProperty AnisotropicParam;
+
+            //@@@ PBR use
+            // Surface Option Props
+            public MaterialProperty workflowMode;
+
+            // Surface Input Props
+            public MaterialProperty metallic;
+            public MaterialProperty specColor;
+            public MaterialProperty metallicGlossMap;
+            public MaterialProperty specGlossMap;
+            public MaterialProperty smoothness;
+            public MaterialProperty smoothnessMapChannel;
+            public MaterialProperty bumpMapProp;
+            public MaterialProperty bumpScaleProp;
+            public MaterialProperty occlusionStrength;
+            public MaterialProperty occlusionMap;
+
+            // Advanced Props
+            public MaterialProperty highlights;
+            public MaterialProperty reflections;
+            //
+
+            public LitStandardProperties(MaterialProperty[] properties)
+            {
+                this.LightModeToggle = BaseShaderGUI.FindProperty("_LightMode", properties, false);
+                this.AlbedoMap = BaseShaderGUI.FindProperty("_AlbedoTexture", properties, false);
+                this.NormalMap = BaseShaderGUI.FindProperty("_BumpMap", properties, false);
+                this.SpecularMap = BaseShaderGUI.FindProperty("_SpecularTexture", properties, false);
+                this.EmissionMap = BaseShaderGUI.FindProperty("_EmissionTexture", properties, false);
+
+                this.Albedo = BaseShaderGUI.FindProperty("_Albedo", properties, false);
+                this.NormalScale = BaseShaderGUI.FindProperty("_BumpScale", properties, false);
+                this.Gloss = BaseShaderGUI.FindProperty("_Gloss", properties, false);
+                //this.AnisotropicParam = BaseShaderGUI.FindProperty("_Anisotropic", parameters, false);
+
+                //@@@PBR use
+                // Surface Option Props
+                workflowMode = BaseShaderGUI.FindProperty("_WorkflowMode", properties, false);
+                // Surface Input Props
+                metallic = BaseShaderGUI.FindProperty("_Metallic", properties);
+                specColor = BaseShaderGUI.FindProperty("_SpecColor", properties, false);
+                metallicGlossMap = BaseShaderGUI.FindProperty("_MetallicGlossMap", properties);
+                specGlossMap = BaseShaderGUI.FindProperty("_SpecGlossMap", properties, false);
+                smoothness = BaseShaderGUI.FindProperty("_Smoothness", properties, false);
+                smoothnessMapChannel = BaseShaderGUI.FindProperty("_SmoothnessTextureChannel", properties, false);
+                bumpMapProp = BaseShaderGUI.FindProperty("_BumpMap", properties, false);
+                bumpScaleProp = BaseShaderGUI.FindProperty("_BumpScale", properties, false);
+                occlusionStrength = BaseShaderGUI.FindProperty("_OcclusionStrength", properties, false);
+                occlusionMap = BaseShaderGUI.FindProperty("_OcclusionMap", properties, false);
+                // Advanced Props
+                highlights = BaseShaderGUI.FindProperty("_SpecularHighlights", properties, false);
+                reflections = BaseShaderGUI.FindProperty("_EnvironmentReflections", properties, false);
+                //
+            }
+
+        }
+
+        public static void Inputs(LitStandardProperties properties, MaterialEditor materialEditor, Material material)
+        {
+            EditorGUI.BeginChangeCheck();
+            var LightModeToggleIndex = (int)properties.LightModeToggle.floatValue;
+            LightModeToggleIndex = EditorGUILayout.Popup(Styles.LightModeTip, LightModeToggleIndex, Styles.LightModeToggleText);
+            if (EditorGUI.EndChangeCheck())
+                properties.LightModeToggle.floatValue = LightModeToggleIndex;
+
+            if ((LightMode)LightModeToggleIndex == LightMode.Classic_Lighting)
+            {
+                var hasAlbedoMap = properties.AlbedoMap.textureValue != null;
+                materialEditor.TexturePropertySingleLine(
+                    hasAlbedoMap ? Styles.AlbedoMapTip : Styles.AlbedoTip,
+                    properties.AlbedoMap,
+                    properties.Albedo
+                );
+
+                materialEditor.TexturePropertySingleLine(
+                    Styles.SpecularMapTip,
+                    properties.SpecularMap,
+                    properties.Gloss
+                );
+
+                materialEditor.TexturePropertySingleLine(
+                    Styles.NormalMapTip,
+                    properties.NormalMap,
+                    properties.NormalScale
+                );
+                materialEditor.TexturePropertySingleLine(Styles.EmissionMapTip, properties.EmissionMap);
+            }
+
+
+            if ((LightMode)LightModeToggleIndex == LightMode.PBR)
+            {
+                var hasAlbedoMap = properties.AlbedoMap.textureValue != null;
+                materialEditor.TexturePropertySingleLine(
+                    hasAlbedoMap ? Styles.AlbedoMapTip : Styles.AlbedoTip,
+                    properties.AlbedoMap,
+                    properties.Albedo
+                );
+                DoMetallicSpecularArea(properties, materialEditor, material);
+                BaseShaderGUI.DrawNormalArea(materialEditor, properties.bumpMapProp, properties.bumpScaleProp);
+
+                if (properties.occlusionMap != null)
+                {
+                    materialEditor.TexturePropertySingleLine(Styles.occlusionText, properties.occlusionMap,
+                        properties.occlusionMap.textureValue != null ? properties.occlusionStrength : null);
+                }
+            }
+        }
+
+        public static void DoMetallicSpecularArea(LitStandardProperties properties, MaterialEditor materialEditor, Material material)
+        {
+            string[] smoothnessChannelNames;
+            bool hasGlossMap = false;
+            if (properties.workflowMode == null ||
+                (WorkflowMode)properties.workflowMode.floatValue == WorkflowMode.Metallic)
+            {
+                hasGlossMap = properties.metallicGlossMap.textureValue != null;
+                smoothnessChannelNames = Styles.metallicSmoothnessChannelNames;
+                materialEditor.TexturePropertySingleLine(Styles.metallicMapText, properties.metallicGlossMap,
+                    hasGlossMap ? null : properties.metallic);
+            }
+            else
+            {
+                hasGlossMap = properties.specGlossMap.textureValue != null;
+                smoothnessChannelNames = Styles.specularSmoothnessChannelNames;
+                BaseShaderGUI.TextureColorProps(materialEditor, Styles.specularMapText, properties.specGlossMap,
+                    hasGlossMap ? null : properties.specColor);
+            }
+            EditorGUI.indentLevel++;
+            DoSmoothness(properties, material, smoothnessChannelNames);
+            EditorGUI.indentLevel--;
+        }
+
+        public static void DoSmoothness(LitStandardProperties properties, Material material, string[] smoothnessChannelNames)
+        {
+            var opaque = ((BaseShaderGUI.SurfaceType)material.GetFloat("_Surface") ==
+                          BaseShaderGUI.SurfaceType.Opaque);
+            EditorGUI.indentLevel++;
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.showMixedValue = properties.smoothness.hasMixedValue;
+            var smoothness = EditorGUILayout.Slider(Styles.smoothnessText, properties.smoothness.floatValue, 0f, 1f);
+            if (EditorGUI.EndChangeCheck())
+                properties.smoothness.floatValue = smoothness;
+            EditorGUI.showMixedValue = false;
+
+            if (properties.smoothnessMapChannel != null) // smoothness channel
+            {
+                EditorGUI.indentLevel++;
+                EditorGUI.BeginDisabledGroup(!opaque);
+                EditorGUI.BeginChangeCheck();
+                EditorGUI.showMixedValue = properties.smoothnessMapChannel.hasMixedValue;
+                var smoothnessSource = (int)properties.smoothnessMapChannel.floatValue;
+                if (opaque)
+                    smoothnessSource = EditorGUILayout.Popup(Styles.smoothnessMapChannelText, smoothnessSource,
+                        smoothnessChannelNames);
+                else
+                    EditorGUILayout.Popup(Styles.smoothnessMapChannelText, 0, smoothnessChannelNames);
+                if (EditorGUI.EndChangeCheck())
+                    properties.smoothnessMapChannel.floatValue = smoothnessSource;
+                EditorGUI.showMixedValue = false;
+                EditorGUI.EndDisabledGroup();
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+        }
+
+        public static SmoothnessMapChannel GetSmoothnessMapChannel(Material material)
+        {
+            int ch = (int)material.GetFloat("_SmoothnessTextureChannel");
+            if (ch == (int)SmoothnessMapChannel.AlbedoAlpha)
+                return SmoothnessMapChannel.AlbedoAlpha;
+
+            return SmoothnessMapChannel.SpecularMetallicAlpha;
+        }
+
+        public static void SetMaterialKeywords(Material material)
+        {
+            // Note: keywords must be based on Material value not on MaterialProperty due to multi-edit & material animation
+            // (MaterialProperty value might come from renderer material property block)
+            var hasGlossMap = false;
+            var isSpecularWorkFlow = false;
+            var opaque = ((BaseShaderGUI.SurfaceType)material.GetFloat("_Surface") ==
+                          BaseShaderGUI.SurfaceType.Opaque);
+
+            if (material.HasProperty("_LightMode"))
+            {
+                LightMode lightModeType = (LightMode)material.GetInt("_LightMode");
+                CoreUtils.SetKeyword(material, "_LightMode_Classic_Lighting", lightModeType==LightMode.Classic_Lighting);
+                CoreUtils.SetKeyword(material, "_LightMode_PBR", lightModeType==LightMode.PBR);
+            }
+
+            var hasSpecularMap = material.GetTexture("_SpecularTexture") != null;
+            CoreUtils.SetKeyword(material, "_SPECULARMAP",hasSpecularMap);
+
+            if (material.HasProperty("_WorkflowMode"))
+            {
+                isSpecularWorkFlow = (WorkflowMode)material.GetFloat("_WorkflowMode") == WorkflowMode.Specular;
+                if (isSpecularWorkFlow)
+                    hasGlossMap = material.GetTexture("_SpecGlossMap") != null;
+                else
+                    hasGlossMap = material.GetTexture("_MetallicGlossMap") != null;
+            }
+            else
+            {
+                hasGlossMap = material.GetTexture("_MetallicGlossMap") != null;
+            }
+
+            CoreUtils.SetKeyword(material, "_SPECULAR_SETUP", isSpecularWorkFlow);
+
+            CoreUtils.SetKeyword(material, "_METALLICSPECGLOSSMAP", hasGlossMap);
+
+            if (material.HasProperty("_SpecularHighlights"))
+                CoreUtils.SetKeyword(material, "_SPECULARHIGHLIGHTS_OFF",
+                    material.GetFloat("_SpecularHighlights") == 0.0f);
+            if (material.HasProperty("_EnvironmentReflections"))
+                CoreUtils.SetKeyword(material, "_ENVIRONMENTREFLECTIONS_OFF",
+                    material.GetFloat("_EnvironmentReflections") == 0.0f);
+            if (material.HasProperty("_OcclusionMap"))
+                CoreUtils.SetKeyword(material, "_OCCLUSIONMAP", material.GetTexture("_OcclusionMap"));
+
+            if (material.HasProperty("_SmoothnessTextureChannel"))
+            {
+                CoreUtils.SetKeyword(material, "_SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A",
+                    GetSmoothnessMapChannel(material) == SmoothnessMapChannel.AlbedoAlpha && opaque);
+            }
+        }
+
+    }
+}
---
+++ Shaders/2.4/LitForwardPass.hlsl
@@ -0,0 +1,227 @@
+#ifndef UNIVERSAL_FORWARD_LIT_PASS_INCLUDED
+    #define UNIVERSAL_FORWARD_LIT_PASS_INCLUDED
+
+    #include "StandardLighting.hlsl"
+
+    struct Attributes
+    {
+        float4 positionOS   : POSITION;
+        float3 normalOS     : NORMAL;
+        float4 tangentOS    : TANGENT;
+        float2 texcoord     : TEXCOORD0;
+        float2 lightmapUV   : TEXCOORD1;
+        UNITY_VERTEX_INPUT_INSTANCE_ID
+    };
+
+    struct Varyings
+    {
+        float2 uv                       : TEXCOORD0;
+        DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 1);
+
+        #if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+            float3 positionWS               : TEXCOORD2;
+        #endif
+
+        float3 normalWS                 : TEXCOORD3;
+
+        float4 tangentWS                : TEXCOORD4;    // xyz: tangent, w: sign
+
+        float3 bitangentWS              :TEXCOORD8;
+
+        float3 viewDirWS                : TEXCOORD5;
+
+        half4 fogFactorAndVertexLight   : TEXCOORD6; // x: fogFactor, yzw: vertex light
+
+        #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+            float4 shadowCoord              : TEXCOORD7;
+        #endif
+
+        float4 positionCS               : SV_POSITION;
+        float4 ssPos					:VAR_SCRPOS;
+
+        UNITY_VERTEX_INPUT_INSTANCE_ID
+        UNITY_VERTEX_OUTPUT_STEREO
+    };
+
+    void InitializeInputData(Varyings input, half3 normalTS, out InputData inputData)
+    {
+        inputData = (InputData)0;
+
+        #if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+            inputData.positionWS = input.positionWS;
+        #endif
+
+        half3 viewDirWS = SafeNormalize(input.viewDirWS);
+        #ifdef _NORMALMAP 
+            float sgn = input.tangentWS.w;      // should be either +1 or -1
+            float3 bitangent = sgn * cross(input.normalWS.xyz, input.tangentWS.xyz);
+            inputData.normalWS = TransformTangentToWorld(normalTS, half3x3(input.tangentWS.xyz, bitangent.xyz, input.normalWS.xyz));
+        #else
+            inputData.normalWS = input.normalWS;
+        #endif
+
+        inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+        inputData.viewDirectionWS = viewDirWS;
+
+        #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+            inputData.shadowCoord = input.shadowCoord;
+        #elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+            inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+        #else
+            inputData.shadowCoord = float4(0, 0, 0, 0);
+        #endif
+
+        inputData.fogCoord = input.fogFactorAndVertexLight.x;
+        inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+        inputData.bakedGI = SAMPLE_GI(input.lightmapUV, input.vertexSH, inputData.normalWS);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //                  Vertex and Fragment functions                            //
+    ///////////////////////////////////////////////////////////////////////////////
+
+    float4x4 _RowAccess={1,1,1,1,
+        1,1,1,1,
+        1,1,1,1,
+    1,1,1,1};
+
+    // Used in Standard (Physically Based) shader
+    Varyings LitPassVertex(Attributes input)
+    {
+        Varyings output = (Varyings)0;
+
+        UNITY_SETUP_INSTANCE_ID(input);
+        UNITY_TRANSFER_INSTANCE_ID(input, output);
+        UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+        VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS.xyz);
+        
+        // normalWS and tangentWS already normalize.
+        // this is required to avoid skewing the direction during interpolation
+        // also required for per-vertex lighting and SH evaluation
+        VertexNormalInputs normalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+        float3 viewDirWS = GetCameraPositionWS() - vertexInput.positionWS;
+        half3 vertexLight = VertexLighting(vertexInput.positionWS, normalInput.normalWS);
+        half fogFactor = ComputeFogFactor(vertexInput.positionCS.z);
+
+        output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
+
+        // already normalized from normal transform to WS.
+        output.normalWS = normalInput.normalWS;
+        output.viewDirWS = viewDirWS;
+        // #ifdef normal map canceled
+
+        #ifdef _NORMALMAP
+            real sign = input.tangentOS.w * GetOddNegativeScale();
+            output.tangentWS = half4(normalInput.tangentWS.xyz, sign);
+            output.bitangentWS = cross(output.normalWS, output.tangentWS) * sign;
+        #else
+            output.tangentWS.xyz =normalInput.tangentWS.xyz;
+            output.bitangentWS =normalInput.bitangentWS;
+        #endif
+
+
+        OUTPUT_LIGHTMAP_UV(input.lightmapUV, unity_LightmapST, output.lightmapUV);
+        OUTPUT_SH(output.normalWS.xyz, output.vertexSH);
+
+        output.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
+
+        #if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+            output.positionWS = vertexInput.positionWS;
+        #endif
+
+        #if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+            output.shadowCoord = GetShadowCoord(vertexInput);
+        #endif
+
+        output.positionCS = vertexInput.positionCS;
+        output.ssPos.xy = vertexInput.positionCS.xy*0.5+0.5*float2( vertexInput.positionCS.w, vertexInput.positionCS.w);
+        output.ssPos.zw = vertexInput.positionCS.zw;
+        return output;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //                  anisotropic  functions                                   //
+    ///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+    half4 LitPassFragment_Standard(Varyings input) : SV_Target{
+        UNITY_SETUP_INSTANCE_ID(input);
+        UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+        SurfaceData surfaceData;
+        InitializeStandardLitSurfaceData(input.uv, surfaceData);
+
+        InputData inputData;
+        InitializeInputData(input, surfaceData.normalTS, inputData);
+
+        float3 N=inputData.normalWS;
+
+        half4 color;
+        #ifdef _LightMode_Classic_Lighting
+            color =UniversalFragmentBasic_lighting(inputData, surfaceData.albedo, surfaceData.metallic,
+            surfaceData.specular, surfaceData.smoothness, surfaceData.occlusion, surfaceData.emission, surfaceData.alpha);
+        #else
+            color = UniversalFragmentPBR_Standard(inputData, surfaceData.albedo, surfaceData.metallic,
+            surfaceData.specular, surfaceData.smoothness, surfaceData.occlusion, surfaceData.emission, surfaceData.alpha);
+        #endif
+        
+        color.rgb = MixFog(color.rgb, inputData.fogCoord);
+        //color.rgb = N;
+        color.a = OutputAlpha(color.a);
+        
+        return color;
+    }
+
+
+
+
+
+
+    // Used in Standard (Physically Based) shader
+    half4 LitPassFragment(Varyings input) : SV_Target
+    {
+        UNITY_SETUP_INSTANCE_ID(input);
+        UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+        SurfaceData surfaceData;
+        InitializeStandardLitSurfaceData(input.uv, surfaceData);
+
+        InputData inputData;
+        InitializeInputData(input, surfaceData.normalTS, inputData);
+
+        half4 color = UniversalFragmentPBR(inputData, surfaceData.albedo, surfaceData.metallic, surfaceData.specular, surfaceData.smoothness, surfaceData.occlusion, surfaceData.emission, surfaceData.alpha);
+        
+        color.rgb = MixFog(color.rgb, inputData.fogCoord);
+        color.a = OutputAlpha(color.a);
+        return color;
+
+        /* 	
+        input.ssPos.xy/=input.ssPos.w;
+        #ifdef UNITY_STARTS_AT_Top
+            input.ssPos.y=1-input.ssPos.y;
+        #endif
+        float DITHER_THRESHOLDS[16] =
+        {
+            1.0 / 17.0,  9.0 / 17.0,  3.0 / 17.0, 11.0 / 17.0,
+            13.0 / 17.0,  5.0 / 17.0, 15.0 / 17.0,  7.0 / 17.0,
+            4.0 / 17.0, 12.0 / 17.0,  2.0 / 17.0, 10.0 / 17.0,
+            16.0 / 17.0,  8.0 / 17.0, 14.0 / 17.0,  6.0 / 17.0
+        };
+        float2 pos=input.ssPos.xy*_ScreenParams.xy;
+        uint index = (uint(pos.x) % 4) * 4 + uint(pos.y) % 4;
+
+        
+        clip(0.3 - DITHER_THRESHOLDS[index]);
+
+        return color; 
+        */
+    }
+
+#endif
